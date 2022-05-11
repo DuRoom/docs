c43cb0929ef3a4ebe453616fc8f6afe530ead14c
NKDuy <kn145660@gmail.com>
2022-05-11 17:07:55 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   favicon: 'img/favicon.ico',
   organizationName: 'DuRoom',
   projectName: 'duroom',
+  trailingSlash: false,
 
   i18n: {
     defaultLocale: 'en',
